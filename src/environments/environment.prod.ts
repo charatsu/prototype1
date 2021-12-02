af5b85b905a9b6edaae163c90eff48ed57baa087
Tommy <tommy.pham@contemi.com.vn>
2021-12-02 10:19:15 +0700

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  amplify: {
+    region: 'eu-west-2',
+    userPoolId: 'eu-west-2_tz6ym19LI',
+    userPoolWebClientId: '2fkb6lfipqkm0jvbptv6n60ehv',
+    redirectSignIn: 'https://app.seamless.insure',
+    redirectSignOut: 'https://app.seamless.insure',
+    oauthDomain: 'seamless-tenant-users.auth.eu-west-2.amazoncognito.com',
+    authorizedScopes: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+    responseType: 'code'
+  }
 };
